d2b9ecf4ceb853b8fbe4e4e02114fadd3084215e
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-15 23:33:03 -0400

    Add suport for inline library importation (dom importation).

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+//webpack.config.js
+const path = require('path');
+const {version} = require('./package.json');
+
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./dist/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, `./versions/${version}/`),
+    filename: "index.js",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
