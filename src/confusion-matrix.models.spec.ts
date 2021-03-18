d59221c9901f4fffcb82e3a9142786fd2bfae698
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-17 20:09:14 -0400

    Add matrix transpose feature.

--- src/confusion-matrix.models.spec.ts
+++ src/confusion-matrix.models.spec.ts
@@ -573,6 +573,31 @@ describe("Confusion matrix model test suite", () => {
         expect(value).toBe(expectedWeightMatrixF1ScoreValue);
     });
 
+    it("Can transpose matrix.", () => {
+        let labels = ["a", "b", "c", "d"];
+        let matrix = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+        let confusionMatrix = new ConfusionMatrix({ labels, matrix });
+        confusionMatrix.transpose();
+        expect(confusionMatrix.matrix).toEqual([
+            [1, 5, 9, 13],
+            [2, 6, 10, 14],
+            [3, 7, 11, 15],
+            [4, 8, 12, 16]
+        ]);
+        confusionMatrix.transpose();
+        expect(confusionMatrix.matrix).toEqual(matrix);
+        labels = ["a"];
+        matrix = [[1]];
+        confusionMatrix = new ConfusionMatrix({ labels, matrix });
+        confusionMatrix.transpose();
+        expect(confusionMatrix.matrix).toEqual(matrix);
+    });
+
 });
 
 class TestsHelper {
