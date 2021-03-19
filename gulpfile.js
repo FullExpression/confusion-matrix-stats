86d73d0080905fdd57bfab710751876ccecfc59f
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-19 15:58:31 -0400

    Add gulp tast to deploy webpage #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 const gulp = require('gulp');
+const ghpages = require('gh-pages');
+
 exports.copyDistFiles = () => {
       return gulp.src('package.json')
     .pipe(gulp.dest('./dist'));
 }
+
+exports.publishWebSite = (callback) => {
+    ghpages.publish('website', (err) => {
+        if (error) {
+            console.error(err);
+        }
+        callback();
+    });
+}
+    
+ 
+
